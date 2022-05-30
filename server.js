ae62a54a584a474acef2f0759e379252ea1965c6
l bates childs <labchilds@gmail.com>
2022-05-30 17:31:12 -0400

    add server stuff and form submit handler

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 const path = require("path");
 
-
 const PORT = process.env.PORT || 3001;
 const app = express();
-const publicPath = path.join(__dirname, "./public");
+const publicPath = path.join(__dirname, "public");
 
 // middleware
 app.use(cors());
@@ -19,8 +18,12 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(publicPath));
 }
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+
 app.get("*", (req, res) => {
-  res.sendFile(path.join(`${publicPath}/index.html`));
+  res.sendFile(path.join(publicPath, "index.html"));
 });
 
 app.listen(PORT, () => {
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,13 +2,36 @@ import React, { useState } from "react";
 
 const ContactForm = () => {
   // set form state
-  const [formStatus, setFormStatus] = useState("Submit");
+  const [formStatus, setFormStatus] = useState("submit");
   const [rsvpChoice, setRSVPChoice] = useState("");
   // handle submit (RSVP) and send as email
   // async to fetch for response to mailer, read docs
   const handleRSVP = async (e) => {
     e.preventDefault();
     console.log("rsvp'd");
+    setFormStatus("sending...");
+
+    const { name, phone, rsvp, message } = e.target.elements;
+    let details = {
+        name: name.value,
+        phone: phone.value,
+        rsvp: rsvp.value,
+        message: message.value
+    };
+    console.log(details);
+
+    let response = await fetch("/contact", {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(details)
+    });
+    console.log(response);
+
+    setFormStatus("submit");
+    let result = await response.json();
+    alert(result.status);
   };
 
   // return jsx
@@ -26,16 +49,21 @@ const ContactForm = () => {
         <label htmlFor="rsvp">will you be celebrating with us?</label>
         <select
           name="rsvp"
-          id="rsvp-select"
+          id="rsvp"
           defaultValue={rsvpChoice}
           onChange={(e) => setRSVPChoice(e.target.value)}
+          required
         >
           <option value={""} disabled>
             yes or no...
           </option>
           <option value={true}>wouldn't miss it for the world &#x1F973;</option>
-          <option value={false}>i'm busy but drowning in fomo &#x1F62D;</option>
+          <option value={false}>i'm busy, but drowning in fomo &#x1F62D;</option>
         </select>
+      </div>
+      <div>
+          <label htmlFor="message">anything else you you wanna say?</label>
+          <textarea id="message" name="message" />
       </div>
       <button type="submit">{formStatus}</button>
     </form>
