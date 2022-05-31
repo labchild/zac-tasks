f18f737b0b488a81c11bd357edfc43718c723ddf
l bates childs <labchilds@gmail.com>
2022-05-30 23:38:05 -0400

    add nodemailer, oauth, post route works

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
@@ -13,10 +14,56 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// mailer logic
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PW,
+    clientId: process.env.OAUTH_CLIENTID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+  },
+});
+
+transporter.verify((err, success) => {
+  err ? console.log(err) : console.log(`ready for messages : ${success}`);
+});
+
 // app.use('/', router);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(publicPath));
 }
+
+// route to send emails
+app.post("/rsvp", (req, res) => {
+  console.log("sending...");
+
+  console.log(req.body);
+  console.log(req.body.email);
+  let mailOptions = {
+    from: req.body.email,
+    to: process.env.EMAIL,
+    subject: `zac party rsvp from: ${req.body.name}`,
+    html: `
+    <h4>${req.body.name} is attending: ${req.body.rsvp ? "yep!" : "too bad"}</h4>
+    <p>email: <a href="mailto:${req.body.email}">${req.body.email}</a></p>
+    <p>notes: ${req.body.message}</p>
+    `,
+  };
+
+  console.log(mailOptions);
+
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log("error: " + err);
+    } else {
+      console.log("email sent!");
+      res.json({ status: "email sent" });
+    }
+  });
+});
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,34 +2,50 @@ import React, { useState } from "react";
 
 const ContactForm = () => {
   // set form state
-  const [formStatus, setFormStatus] = useState("submit");
-  const [rsvpChoice, setRSVPChoice] = useState("");
+  const [buttonState, setbuttonState] = useState("submit");
+  // const [rsvpChoice, setRSVPChoice] = useState("");
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+    rsvp: "",
+  });
+
+  // handle form state change
+  function handleStateChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+    console.log(formData);
+  }
+
   // handle submit (RSVP) and send as email
   // async to fetch for response to mailer, read docs
   const handleRSVP = async (e) => {
     e.preventDefault();
     console.log("rsvp'd");
-    setFormStatus("sending...");
+    setbuttonState("sending...");
 
-    const { name, phone, rsvp, message } = e.target.elements;
+    const { name, email, rsvp, message } = e.target.elements;
     let details = {
-        name: name.value,
-        phone: phone.value,
-        rsvp: rsvp.value,
-        message: message.value
+      name: name.value,
+      email: email.value,
+      rsvp: rsvp.value,
+      message: message.value,
     };
     console.log(details);
 
-    let response = await fetch("/contact", {
-        method: "POST",
-        headers: {
-            "Content-Type":"application/json"
-        },
-        body: JSON.stringify(details)
+    let response = await fetch("/rsvp", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ details }),
     });
     console.log(response);
 
-    setFormStatus("submit");
+    setbuttonState("submit");
     let result = await response.json();
     alert(result.status);
   };
@@ -39,33 +55,51 @@ const ContactForm = () => {
     <form onSubmit={handleRSVP}>
       <div>
         <label htmlFor="name">who are you? (you gotta tell me)</label>
-        <input name="name" id="name" type="text" required />
+        <input
+          placeholder="name"
+          name="name"
+          id="name"
+          type="text"
+          onChange={handleStateChange}
+          value={formData.name}
+          required
+        />
       </div>
       <div>
-        <label htmlFor="phone">how can i bug you? (not required, but helpful!)</label>
-        <input name="phone" id="phone" type="tel" />
+        <label htmlFor="email">your deets: </label>
+        <input
+          placeholder="email"
+          name="email"
+          id="email"
+          type="email"
+          onChange={handleStateChange}
+          value={formData.email}
+          required
+        />
       </div>
       <div>
         <label htmlFor="rsvp">will you be celebrating with us?</label>
         <select
           name="rsvp"
           id="rsvp"
-          defaultValue={rsvpChoice}
-          onChange={(e) => setRSVPChoice(e.target.value)}
+          value={formData.rsvp}
+          onChange={handleStateChange}
           required
         >
           <option value={""} disabled>
             yes or no...
           </option>
           <option value={true}>wouldn't miss it for the world &#x1F973;</option>
-          <option value={false}>i'm busy, but drowning in fomo &#x1F62D;</option>
+          <option value={false}>
+            i'm busy, but drowning in fomo &#x1F62D;
+          </option>
         </select>
       </div>
       <div>
-          <label htmlFor="message">anything else you you wanna say?</label>
-          <textarea id="message" name="message" />
+        <label htmlFor="message">anything else you you wanna say?</label>
+        <textarea id="message" name="message" />
       </div>
-      <button type="submit">{formStatus}</button>
+      <button type="submit">{buttonState}</button>
     </form>
   );
 };
